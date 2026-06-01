90c8d591c6f4f5b6be06a0da279fbec77ac50d78
Prince Baah Dadzie <mrprincewinez1@gmail.com>
2026-06-01 20:22:40 +0000

    Disable browser PostHog analytics

--- instrumentation-client.ts
+++ instrumentation-client.ts
@@ -1,18 +1,1 @@
-import posthog from "posthog-js";
-
-const posthogToken = process.env.NEXT_PUBLIC_POSTHOG_PROJECT_TOKEN;
-
-if (posthogToken) {
-  posthog.init(posthogToken, {
-    api_host: "/ingest",
-    ui_host: "https://us.posthog.com",
-    defaults: "2026-01-30",
-    capture_exceptions: true,
-    disable_external_dependency_loading: true,
-    debug: process.env.NODE_ENV === "development",
-    loaded: () => {
-      window.localStorage.removeItem("_postHogToolbarParams");
-      window.sessionStorage.removeItem("toolbarParams");
-    },
-  });
-}
+export {};
